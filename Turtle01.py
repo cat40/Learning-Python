aa6d6845ec4dd70e6364c9c7d5c38b1e42409d9a
zerostarm <zerostarm@gmail.com>
2016-09-10 21:36:53 -0400

    Turtle01
    A simple Turtle drawing program.

---
+++ Turtle01.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep 10 17:45:03 2016
+
+@author: Backup
+"""
+import math
+
+import turtle
+turtle.RawPen(turtle.Screen())
+p = (math.pi*3/4) 
+turtle.radians()
+turtle.pu()
+turtle.setpos(-150, (300*5/24) + 20)
+turtle.pd()
+
+for b in range(0,8,1):
+    turtle.fd(300)
+    turtle.right(p)
+
+turtle.pu()
+turtle.setpos(-70,20)
+turtle.left(p*8/3)
+turtle.pd()
+turtle.write("Timmy is a Teacher now", True, "left",("Times New Roman", 12, "normal"))
+
+import time
+time.sleep(10)
+
+import IPython
+app = IPython.Application.instance()
+app.kernel.do_shutdown(True)  
